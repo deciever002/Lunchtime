ec179cc759fd7722ee1acd8b39a140354755d558
deciever002 <112121338+deciever002@users.noreply.github.com>
2023-01-16 14:16:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Setup up a React Code from Scratch
+// to which displays "Breakfast Time" if
+// the current time is before 12:00 in
+// the morning, "Lunch Time" if the time is
+// between 12:00 to 18:00, and "Dinner Time".
+
+// if the current time is after 18:00.
+// Sample Output at 15:00.
